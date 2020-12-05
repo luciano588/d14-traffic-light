8df360441f7f0db14ddd2bdb1c9b9ef6bd56589c
luciano588 <luciano588.alm@gmail.com>
2020-12-05 17:18:18 +0000

    commit 1

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,17 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 export function Home() {
+	const [color, setColor] = useState(null);
 	return (
 		<div>
 			<div className="topStick mx-auto" />
 			<div className="stopLight mx-auto d-flex flex-column align-items-center justify-content-center">
-				<div className="red light"> </div>
-				<div className="yellow light"> </div>
-				<div className="green light"> </div>
+				<div
+					className={
+						color === "red" ? "red selected light" : "red light"
+					}
+					onClick={() => setColor("red")}
+				/>
+				<div
+					className={
+						color === "yellow"
+							? "yellow selected light"
+							: "yellow light"
+					}
+					onClick={() => setColor("yellow")}
+				/>
+
+				<div
+					className={
+						color === "green"
+							? "green selected light"
+							: "green light"
+					}
+					onClick={() => setColor("green")}
+				/>
 			</div>
 		</div>
 	);
